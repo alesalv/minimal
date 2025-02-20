bd59da4b3a75c5b009bf831abdbe7e5d8ad0591a
Alessio Salvadorini <alesalv.dev@gmail.com>
2025-02-20 09:02:56 +0200

    Extract method

--- example/lib/src/chroma_counter/notifiers/chroma_counter_notifier.dart
+++ example/lib/src/chroma_counter/notifiers/chroma_counter_notifier.dart
@@ -6,26 +6,28 @@ import '../views/ui_states/chroma_counter_ui_state.dart';
 class ChromaCounterNotifier extends MMNotifier<ChromaCounterUIState> {
   ChromaCounterNotifier() : super(const ChromaCounterUIState());
 
-  final _random = math.Random();
-
-  BorderRadius _randomBorderRadius() => BorderRadius.only(
-        topLeft: Radius.circular(_random.nextDouble() * 100),
-        topRight: Radius.circular(_random.nextDouble() * 100),
-        bottomLeft: Radius.circular(_random.nextDouble() * 100),
-        bottomRight: Radius.circular(_random.nextDouble() * 100),
-      );
+  static final _random = math.Random();
 
   void nextMetamorph() => notify(
         state.copyWith(
           count: state.count + 1,
-          backgroundColor: Color.fromRGBO(
-            _random.nextInt(256),
-            _random.nextInt(256),
-            _random.nextInt(256),
-            1,
-          ),
-          borderRadius: _randomBorderRadius(),
+          backgroundColor: _randomColor(),
+          borderRadius: _randomRadius(),
         ),
+      );
+
+  Color _randomColor() => Color.fromRGBO(
+        _random.nextInt(256),
+        _random.nextInt(256),
+        _random.nextInt(256),
+        1,
+      );
+
+  BorderRadius _randomRadius() => BorderRadius.only(
+        topLeft: Radius.circular(_random.nextDouble() * 100),
+        topRight: Radius.circular(_random.nextDouble() * 100),
+        bottomLeft: Radius.circular(_random.nextDouble() * 100),
+        bottomRight: Radius.circular(_random.nextDouble() * 100),
       );
 }
 
